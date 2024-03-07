cd0561306ef4777d4146490a856bf19d396c6100
ckddms6530 <ckddms6530@gmail.com>
2024-03-07 20:22:45 +0900

    product api 테스트 추

---
+++ shoppingmall-api/src/test/java/com/nhnacademy/shoppingmall/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.shoppingmall.service.impl;
+
+import com.nhnacademy.shoppingmall.dto.ProductDto;
+import com.nhnacademy.shoppingmall.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Test
+    void getProductsSummary() {
+        Page<ProductDto.ProductSummaryResponse> productSummaryResponse = Page.empty();
+        when(productRepository.findProductSummaryBySearchOption(any(), anyString(), anyLong())).thenReturn(productSummaryResponse);
+        Pageable pageable = PageRequest.of(0, 9);
+        productService.getProductsSummary(pageable, 1L, "keyword");
+
+        verify(productRepository, times(1)).findProductSummaryBySearchOption(any(), anyString(), anyLong());
+    }
+}
