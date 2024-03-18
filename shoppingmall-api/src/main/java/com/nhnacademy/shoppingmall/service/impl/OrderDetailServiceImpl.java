f78823791cfad05f7c56767cde9dec44f37d17d5
ckddms6530 <121488797+ckddms6530@users.noreply.github.com>
2024-03-18 12:13:13 +0900

    주문상세 조회 시 없는 주문의 경우 예외처리 추가

--- shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/service/impl/OrderDetailServiceImpl.java
+++ shoppingmall-api/src/main/java/com/nhnacademy/shoppingmall/service/impl/OrderDetailServiceImpl.java
@@ -2,7 +2,9 @@ package com.nhnacademy.shoppingmall.service.impl;
 
 import com.nhnacademy.shoppingmall.dto.OrderDetailDto;
 import com.nhnacademy.shoppingmall.enitiy.OrderDetail;
+import com.nhnacademy.shoppingmall.exception.order.OrderNotFoundException;
 import com.nhnacademy.shoppingmall.repository.OrderDetailRepository;
+import com.nhnacademy.shoppingmall.repository.OrderRepository;
 import com.nhnacademy.shoppingmall.service.OrderDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,14 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class OrderDetailServiceImpl implements OrderDetailService {
-
+    private final OrderRepository orderRepository;
     private final OrderDetailRepository orderDetailRepository;
     @Override
     @Transactional(readOnly = true)
     public List<OrderDetailDto.ReadResponse> getOrderDetails(Long orderId) {
+        if (!orderRepository.existsById(orderId)) {
+            throw new OrderNotFoundException(orderId);
+        }
         List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderId(orderId);
         return orderDetailList.stream()
                 .map(OrderDetailDto.ReadResponse::fromEntity)
